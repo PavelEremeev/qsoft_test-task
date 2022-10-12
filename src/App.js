b577f01d148b8f962b2e75231ea8c00e374e97f9
PavelEremeev <pe19950520@yandex.ru>
2022-10-12 23:12:08 +0300

    Изменение стилей partners. Добавлена разметка header

--- src/App.js
+++ src/App.js
@@ -6,10 +6,21 @@ function App() {
   return (
     <div className="App">
       <header className="header">
-
-
+        <div className='header__wrapper'>
+          <img className='header__logo'/>
+          <img className='header__user-logo'/>
+          <div className='header__search-container'>
+          <input className='header__search-input'>Найти...</input>
+          <img className='header__search-image'/>
+          </div>
+          <div className='header__user-profile'>
+            <p className='header__user-name'></p>
+            <img className='header__user-image'/>
+          </div>
+        </div>
       </header>
       <section className='partners'>
+        <h1 className='partners__title'>НАШИ ПАРТНЁРЫ</h1>
         <div className='partners__container'>
           <div className='partners__block'>
             <img className='partners__image' src={logo1}/>
